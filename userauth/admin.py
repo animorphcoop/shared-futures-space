a04e241fc4e5ecaa459c192c60e930e038e4ccb4
Szczepan Orlowski <sz@animorph.coop>
2021-10-19 15:14:51 +0100

    custom user model and admin view customisation

--- userauth/admin.py
+++ userauth/admin.py
@@ -8,10 +8,12 @@ class CustomUserAdmin(UserAdmin):
     add_form = UserCreationForm
     form = UserChangeForm
     model = CustomUser
-    list_display = ['pk', 'email', 'username', 'first_name', 'last_name']
+    list_display = ['pk', 'email', 'display_name', 'first_name', 'last_name']
     add_fieldsets = UserAdmin.add_fieldsets + (
-        None, {'fields': ('email', 'first_name', 'last_name')})
-    fieldsets = UserAdmin.fieldsets
+        (None, {'fields': ('email', 'first_name', 'last_name', 'display_name', 'year_of_birth', 'post_code')}))
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('display_name', 'year_of_birth', 'post_code')
+                }),)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- userauth/models.py
+++ userauth/models.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class CustomUser(AbstractUser):
-    pass
+    display_name = models.CharField(verbose_name=_("Display name"),
+                                    max_length=30, help_text=_("Will be shown alongside entries"))
+    year_of_birth = models.PositiveIntegerField(verbose_name=_("Year of birth"),
+                                                validators=[MinValueValidator(1900), MaxValueValidator(2021)])
+    post_code = models.CharField(verbose_name=_("Post code"), max_length=8)
+
+    class Meta:
+        ordering = ['display_name']
+
+    def __str__(self):
+        return f"{self.display_name}: {self.first_name} {self.last_name}"
