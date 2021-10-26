df216a08897d81dfeecc1fadb5b39998bbec3107
asa glenister <asa@animorph.coop>
2021-10-26 17:26:32 +0100

    added test for account creation procedure

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,9 @@ from django.conf import settings
 @pytest.fixture(scope='function')
 def test_user(db, django_user_model):
     return django_user_model.objects.create_user(username='test_user', email='test_user@email.com', password='test_password')
+
+@pytest.fixture(scope='session')
+def celery_config():
+  return {'accept_content' : ['pickle'],
+          'task_serializer' : 'pickle',
+          'result_serializer' : 'pickle'}
