9c7b3445e23fb8527dbe9162501c6b775a3d99ba
asa glenister <asa@animorph.coop>
2021-10-27 15:09:46 +0100

    fully type annotated (except tests)

--- sfs/wsgi.py
+++ sfs/wsgi.py
@@ -14,4 +14,4 @@ from django.core.handlers.wsgi import WSGIHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sfs.settings.dev")
 
-application = get_wsgi_application()
+application: WSGIHandler = get_wsgi_application()
