cf9a8fbd2afd5f9738e60b54448fa04fdbf6b34e
asa glenister <asa@animorph.coop>
2021-11-19 13:38:58 +0000

    added ability to edit owned projects

--- apps/project/urls.py
+++ apps/project/urls.py
@@ -1,7 +1,7 @@
 # pyre-strict
 
 from django.urls import path, URLResolver, URLPattern
-from .views import AllProjectsView, ProjectView, MakeProjectView, DeleteMessageView
+from .views import AllProjectsView, ProjectView, MakeProjectView, EditProjectView, DeleteMessageView
 from typing import List, Union
 
 
@@ -9,5 +9,6 @@ urlpatterns: List[Union[URLResolver, URLPattern]] = [
     path('', AllProjectsView.as_view(template_name='all_projects.html'), name='all_projects'),
     path('view/<int:pk>/', ProjectView.as_view(template_name='project.html'), name='view_project'),
     path('new/', MakeProjectView.as_view(template_name='new_project.html'), name='new_project'),
-    path('delete/', DeleteMessageView.as_view(), name='delete_message')
+    path('edit/<int:pk>/', EditProjectView.as_view(template_name='edit_project.html'), name='edit_project'),
+    path('deletemessage/', DeleteMessageView.as_view(), name='delete_message'),
 ]
--- apps/project/views.py
+++ apps/project/views.py
@@ -2,6 +2,7 @@
 
 from django.views.generic.base import TemplateView, View
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import redirect
@@ -15,7 +16,9 @@ class ProjectView(DetailView):
     def post(self, request: WSGIRequest, pk: int) -> HttpResponse:
         if (request.POST['form'] == 'give_support'):
             if (0 == len(ProjectSupport.objects.filter(project=Project.objects.get(id=pk), # pyre-ignore[16]
-                                                       user=request.user))): # pyre-ignore[16]
+                                                       user=request.user)) and # pyre-ignore[16]
+                0 == len(ProjectOwnership.objects.filter(project=Project.objects.get(id=pk), # pyre-ignore[16]
+                                                         user=request.user))): # pyre-ignore[16]
                 support = ProjectSupport(project=Project.objects.get(id=pk),
                                          user=request.user)
                 support.save()
@@ -58,6 +61,25 @@ class MakeProjectView(TemplateView):
         ownership = ProjectOwnership(project=new_project, user=request.user) # pyre-ignore[16]
         ownership.save()
         return redirect(reverse('all_projects'))
+
+class EditProjectView(UpdateView):
+    model = Project
+    fields = ['name', 'description']
+    def post(self, request: WSGIRequest, pk: int) -> HttpResponse:
+        project = Project.objects.get(id=pk)
+        if (0 != len(ProjectOwnership.objects.filter(project=project, user=request.user))):
+            if ('abdicate' in request.POST and request.POST['abdicate'] == 'abdicate'
+                and 1 < len(ProjectOwnership.objects.filter(project=project))):
+                ownership = ProjectOwnership.objects.get(project=project, user=request.user)
+                ownership.delete()
+            project.name = request.POST['name']
+            project.description = request.POST['description']
+            project.save()
+        return redirect(reverse('view_project', args=[pk]))
+    def get_context_data(self, **kwargs: Dict[str,Any]) -> Dict[str,Any]:
+        context = super().get_context_data(**kwargs)
+        context['ownerships'] = ProjectOwnership.objects.filter(project=context['object'])
+        return context
 
 class DeleteMessageView(View):
     def post(self, request: WSGIRequest) -> HttpResponse:
