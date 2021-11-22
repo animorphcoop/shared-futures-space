52dea3e9e0bc06cfa96a7d1dfe3baa8f07fb7ae0
asa glenister <asa@animorph.coop>
2021-11-22 14:35:22 +0000

    solved bizarre pytest error by creating empty migrations folder under apps/project (this makes django makemigrations realise that it's an app, which it only fails to do under pytest for some reason)

--- apps/project/models.py
+++ apps/project/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from userauth.models import CustomUser # pyre-ignore[21]
+import userauth
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
@@ -9,13 +10,13 @@ class Project(models.Model):
 
 class ProjectOwnership(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey('userauth.CustomUser', on_delete=models.CASCADE)
 
 class ProjectSupport(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey('userauth.CustomUser', on_delete=models.CASCADE)
 
 class ProjectMessage(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     message = models.CharField(max_length=2000)
-    user_from = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user_from = models.ForeignKey('userauth.CustomUser', on_delete=models.CASCADE)
