d91c44f2e14eff188c670bcac2b17949fe5d30cd
asa glenister <asa@animorph.coop>
2021-11-24 12:55:21 +0000

    add login_required to some pages

--- apps/userauth/urls.py
+++ apps/userauth/urls.py
@@ -1,16 +1,18 @@
 # pyre-strict
 from django.conf import settings
 from django.urls import include, path, URLResolver, URLPattern
+from django.contrib.auth.decorators import login_required
 from .views import CustomUserUpdateView, CustomUserDeleteView, profile_view, user_request_view, admin_request_view, CustomUserPersonalView
 from typing import List, Union
 
+# !!! when adding new urls, don't forget to make them login_required if appropriate!
 urlpatterns: List[Union[URLResolver, URLPattern]] = [
-    path('request/', user_request_view, name='account_request'),
-    path('managerequests/', admin_request_view, name='account_request_panel'),
-    path('view/', profile_view, name='account_view'),
-    path('data/', CustomUserPersonalView.as_view(template_name='account/data.html'), name='account_data'),
-    path('update/', CustomUserUpdateView.as_view(template_name='account/update.html'), name='account_update'),
-    path('delete/', CustomUserDeleteView.as_view(template_name='account/delete.html'), name='account_delete')
+    path('request/', login_required(user_request_view), name='account_request'),
+    path('managerequests/', login_required(admin_request_view), name='account_request_panel'),
+    path('view/', login_required(profile_view), name='account_view'),
+    path('data/', login_required(CustomUserPersonalView.as_view(template_name='account/data.html')), name='account_data'),
+    path('update/', login_required(CustomUserUpdateView.as_view(template_name='account/update.html')), name='account_update'),
+    path('delete/', login_required(CustomUserDeleteView.as_view(template_name='account/delete.html')), name='account_delete')
 ]
 
 if settings.DEBUG:
