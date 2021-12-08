e2865f4c10e83ee3aabef2933002b9812e867c0e
asa glenister <asa@animorph.coop>
2021-12-08 16:59:24 +0000

    readability + comments in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,17 @@ from django.conf import settings
 
 from project.models import Project
 
+# a user account to use during testing
 @pytest.fixture(scope='function')
 def test_user(db, django_user_model):
     return django_user_model.objects.create_user(username='test_user', email='test_user@email.com', password='test_password', display_name = 'Test User', year_of_birth = 1999, post_code = "AB123CD")
 
+# project to use during testing
 @pytest.fixture(scope='function')
 def test_project(db):
     return Project.objects.create(name = 'some project', description = 'project to do something') 
 
+# make celery work
 @pytest.fixture(scope='session')
 def celery_config():
   return {'accept_content' : ['pickle'],
