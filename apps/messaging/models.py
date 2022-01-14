2b0352afe9904faaa5d5f24dc1229aaf51b36e4d
asa glenister <asa@animorph.coop>
2022-01-14 17:03:37 +0000

    Message and Chat (which can belong to a Project)

---
+++ apps/messaging/models.py
@@ -0,0 +1,16 @@
+# pyre-strict
+
+from userauth.models import CustomUser # pyre-ignore[21]
+from django.db import models
+from uuid import uuid4
+
+class Message(models.Model):
+    uuid = models.UUIDField(default = uuid4, editable = False)
+    timestamp = models.DateTimeField()
+    sender = models.ForeignKey('userauth.CustomUser', null = True, on_delete = models.SET_NULL)
+    text = models.CharField(max_length = 2000)
+    # snippet =
+    chat = models.ForeignKey(Chat, on_delete = models.CASCADE)
+
+class Chat(models.Model):
+    uuid = models.UUIDField(default = uuid4, editable = False)
--- apps/project/models.py
+++ apps/project/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from userauth.models import CustomUser # pyre-ignore[21]
+from messaging.models import Chat # pyre-ignore[21]
 from urllib.parse import quote
 from hashlib import shake_256
 
@@ -27,6 +28,7 @@ class Project(models.Model):
     slug = models.CharField(max_length=100, default='')
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=2000)
+    chat = models.ForeignKey('messaging.Chat')
     def save(self, *args: List[Any], **kwargs: Dict[str,Any]) -> None:
         if (self.slug == ''): # shouldn't happen because created from ideas with existing slugs, but just in case
             self.slug = quote(self.name)[:86] + shake_256(str(self.id).encode()).hexdigest(8) # pyre-ignore[6,8,16] same
