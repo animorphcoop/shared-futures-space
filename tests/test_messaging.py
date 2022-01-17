c7f7cd60cfc57d8725abea56f0fed879b7b5ba2c
asa glenister <asa@animorph.coop>
2022-01-17 15:52:05 +0000

    project interface test

--- tests/test_messaging.py
+++ tests/test_messaging.py
@@ -28,3 +28,26 @@ def test_project_chat_basics(client, test_user, test_project):
     chat_page = client.get(chat_url)
     chat_page_html = bs4.BeautifulSoup(chat_page.content, features='html5lib')
     assert test_user.display_name + ': test message' in chat_page_html.text
+
+def test_project_chat_interface(client, test_user, test_project):
+    chat_url = reverse('project_chat', args=[test_project.slug])
+    ProjectMembership.objects.create(project=test_project, user=test_user, champion=False, owner=False)
+    client.force_login(test_user)
+    for i in range(10):
+        client.post(chat_url, {'message': 'test message ' + str(i)})
+    chat_page = client.get(chat_url)
+    chat_page_html = bs4.BeautifulSoup(chat_page.content, features='html5lib')
+    for i in range(10):
+        assert 'test message ' + str(i) in chat_page_html.text
+    chat_page = client.get(chat_url + '?interval=5')
+    chat_page_html = bs4.BeautifulSoup(chat_page.content, features='html5lib')
+    for i in range(5):
+        assert 'test message ' + str(i) not in chat_page_html.text
+    for i in range(5,10):
+        assert 'test message ' + str(i) in chat_page_html.text
+    chat_page = client.get(chat_url + '?from=5')
+    chat_page_html = bs4.BeautifulSoup(chat_page.content, features='html5lib')
+    for i in range(5):
+        assert 'test message ' + str(i) in chat_page_html.text
+    for i in range(5,10):
+        assert 'test message ' + str(i) not in chat_page_html.text
