ee5ae447f589382138fcda84d06ab17126d61de1
Szczepan Orlowski <sz@animorph.coop>
2022-08-12 16:02:21 +0100

    added resource view return types

--- apps/resources/views.py
+++ apps/resources/views.py
@@ -1,15 +1,17 @@
-from django.core.handlers.wsgi import WSGIRequest
+# pyre-strict
+from django.http.request import HttpRequest
+from django.http import HttpResponse
 
 from .models import Resource
 from django.shortcuts import render
 
-def resource(request: WSGIRequest):
+def resource(request: HttpRequest) -> HttpResponse:
     resources = Resource.objects.all()
     context = {'resources': resources}
     return render(request, 'resources/resources.html', context)
 
 #TODO: search-results template to be merged with list template as they duplicate styles (or export styles)
-def resource_search(request: WSGIRequest):
+def resource_search(request: HttpRequest) -> HttpResponse:
     search_text = request.POST.get('search')
     results = Resource.objects.filter(content__icontains=search_text) #TODO: better query needed
     context = {'results': results}
