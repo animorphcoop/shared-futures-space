6f40a7f609730d70ffc9c80285a6c6e929789a54
asa glenister <asa@animorph.coop>
2022-08-16 15:58:31 +0100

    basic resource upload system

--- apps/core/management/commands/upload.py
+++ apps/core/management/commands/upload.py
@@ -1,6 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
 
-data = {'Resources': [{}]}
+from resources.models import Resource
+
+data = {'Resources': [{'title': 'test resource 1', 'content': 'this resource helps you to do something', 'tags': ['resource', 'useful']},
+                      {'title': 'test resource 2', 'content': 'this resource points you to an external project', 'tags': ['resource', 'organisation']}]}
 
 class Command(BaseCommand):
     help = 'import data'
@@ -9,4 +12,9 @@ class Command(BaseCommand):
         parser.add_argument('n', nargs='?', type=int, default=0)
 
     def handle(self, *args, **options):
-        print(options['n'])
+        for new_resource_data in data['Resources']:
+            print(new_resource_data)
+            new_resource = Resource.objects.create(title=new_resource_data['title'], content=new_resource_data['content'])
+            for tag in new_resource_data['tags']:
+                new_resource.tags.add(tag)
+            new_resource.save()
