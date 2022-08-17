2dc288340985a4158d5b193f3eb6b736a228d4aa
Szczepan Orlowski <sz@animorph.coop>
2022-08-17 18:20:35 +0100

    added mobile navigation base

---
+++ templates/partials/ts/nav.ts
@@ -0,0 +1,9 @@
+const head = document.getElementById('header');
+const nav = document.getElementById('menu');
+
+// Toggle menu
+function navToggle() {
+
+  head.classList.toggle('hidden');
+  nav.classList.toggle('hidden');
+}
---
+++ templates/ts_output/js/nav.js
@@ -0,0 +1,8 @@
+"use strict";
+var head = document.getElementById('header');
+var nav = document.getElementById('menu');
+// Toggle menu
+function navToggle() {
+    head.classList.toggle('hidden');
+    nav.classList.toggle('hidden');
+}
