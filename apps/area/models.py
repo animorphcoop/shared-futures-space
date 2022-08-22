d0e0e057ba37c38b10f4f20bc7d2d3e2d6f2db5d
asa glenister <asa@animorph.coop>
2022-08-22 16:40:48 +0100

    UNFINISHED adding postcodes

--- apps/area/models.py
+++ apps/area/models.py
@@ -4,10 +4,10 @@ from django.db import models
 from uuid import uuid4
 
 
-def Area(models.Model):
+class Area(models.Model):
     uuid: models.UUIDField = models.UUIDField(default=uuid4, editable=False)
     name: models.CharField = models.CharField(max_length = 50)
 
-def PostCode(models.Model):
+class PostCode(models.Model):
     code: models.CharField = models.CharField(max_length = 8)
     area: models.ForeignKey = models.ForeignKey(Area, on_delete = models.CASCADE)
