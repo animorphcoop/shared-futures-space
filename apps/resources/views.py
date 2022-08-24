00dec595047999a7d4ce1f40f42aa6fd5ed9d6db
Szczepan Orlowski <sz@animorph.coop>
2022-08-24 17:31:14 +0100

    combined filtering with taggit

--- apps/resources/views.py
+++ apps/resources/views.py
@@ -7,6 +7,10 @@ from django.shortcuts import render
 from apps.core.helpers.tags_declusterer import objects_tags_cluster_list_overwrite
 from itertools import chain
 
+from django.db.models import Q
+import operator
+import functools
+
 def resource(request: HttpRequest) -> HttpResponse:
     how_tos = objects_tags_cluster_list_overwrite(HowTo.objects.all())
     case_studies = objects_tags_cluster_list_overwrite(CaseStudy.objects.all())
@@ -16,8 +20,24 @@ def resource(request: HttpRequest) -> HttpResponse:
 
 #TODO: search-results template to be merged with list template as they duplicate styles (or export styles)
 def resource_search(request: HttpRequest) -> HttpResponse:
-    print('searchin')
     search_text = request.POST.get('search')
-    results = Resource.objects.filter(content__icontains=search_text) #TODO: better query needed
+    how_tos = objects_tags_cluster_list_overwrite(HowTo.objects.all())
+    case_studies = objects_tags_cluster_list_overwrite(CaseStudy.objects.all())
+
+
+    results = list(chain(
+        how_tos.filter(Q(title__icontains=search_text)
+                       | Q(summary__icontains=search_text)
+                       | Q(tags__name__icontains=search_text)
+                       ),
+
+        case_studies.filter(Q(title__icontains=search_text)
+                       | Q(summary__icontains=search_text)
+                       | Q(tags__name__icontains=search_text)
+                       ),
+    ))
+
     context = {'results': results}
     return render(request, 'resources/partials/search-results.html', context)
+
+
