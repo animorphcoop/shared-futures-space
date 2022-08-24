9a97c261b44a7c930fca8a35eceb0098a768528d
Szczepan Orlowski <sz@animorph.coop>
2022-08-24 17:35:44 +0100

    added tags in filter templates

--- apps/resources/views.py
+++ apps/resources/views.py
@@ -11,6 +11,7 @@ from django.db.models import Q
 import operator
 import functools
 
+
 def resource(request: HttpRequest) -> HttpResponse:
     how_tos = objects_tags_cluster_list_overwrite(HowTo.objects.all())
     case_studies = objects_tags_cluster_list_overwrite(CaseStudy.objects.all())
@@ -18,26 +19,26 @@ def resource(request: HttpRequest) -> HttpResponse:
     context = {'resources': resources}
     return render(request, 'resources/resources.html', context)
 
-#TODO: search-results template to be merged with list template as they duplicate styles (or export styles)
+
 def resource_search(request: HttpRequest) -> HttpResponse:
     search_text = request.POST.get('search')
-    how_tos = objects_tags_cluster_list_overwrite(HowTo.objects.all())
-    case_studies = objects_tags_cluster_list_overwrite(CaseStudy.objects.all())
+    how_tos = HowTo.objects.all()
+    case_studies = CaseStudy.objects.all()
 
+    # filter through key shared fields
+    how_tos.filter(Q(title__icontains=search_text)
+                   | Q(summary__icontains=search_text)
+                   | Q(tags__name__icontains=search_text))
 
-    results = list(chain(
-        how_tos.filter(Q(title__icontains=search_text)
-                       | Q(summary__icontains=search_text)
-                       | Q(tags__name__icontains=search_text)
-                       ),
+    case_studies.filter(Q(title__icontains=search_text)
+                        | Q(summary__icontains=search_text)
+                        | Q(tags__name__icontains=search_text))
 
-        case_studies.filter(Q(title__icontains=search_text)
-                       | Q(summary__icontains=search_text)
-                       | Q(tags__name__icontains=search_text)
-                       ),
-    ))
+    # can iterate over tags only after filtering
+    how_tos = objects_tags_cluster_list_overwrite(how_tos)
+    case_studies = objects_tags_cluster_list_overwrite(case_studies)
+
+    results = list(chain(how_tos, case_studies))
 
     context = {'results': results}
     return render(request, 'resources/partials/search-results.html', context)
-
-
