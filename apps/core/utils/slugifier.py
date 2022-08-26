c0c0d2ccb9795aef66cd8a379ca2151fb78d832e
Szczepan Orlowski <sz@animorph.coop>
2022-08-26 12:54:46 +0100

    restyled resource page a tiny bit

--- apps/core/utils/slugifier.py
+++ apps/core/utils/slugifier.py
@@ -2,7 +2,7 @@ import random
 import string
 
 ALPHANUMERIC_CHARS = string.ascii_lowercase + string.digits
-STRING_LENGTH = 6
+STRING_LENGTH = 4
 
 def generate_random_string(chars=ALPHANUMERIC_CHARS, length=STRING_LENGTH):
     return "".join(random.choice(chars) for _ in range(length))
