dd700423bd37c99af8b3627802ea28b8072ff6c4
asa glenister <asa@animorph.coop>
2022-09-01 15:56:12 +0100

    basic poll voting

--- apps/poll/views.py
+++ apps/poll/views.py
@@ -1,19 +1,30 @@
 # pyre-strict
 
 from django.views.generic.base import TemplateView
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.handlers.wsgi import WSGIRequest
+from django.urls import reverse
 
 from .models import Poll, Vote
 
 class PollView(TemplateView):
+    def post(self, request, uuid):
+        if request.user.is_active and 'choice' in request.POST:
+            poll = Poll.objects.get(uuid = uuid)
+            try:
+                choice = poll.options.index(request.POST['choice']) + 1
+            except ValueError:
+                choice = 0
+            [v.delete() for v in Vote.objects.filter(poll = poll, user = request.user)] # remove previous vote if there is one
+            Vote.objects.create(poll = poll, user = request.user, choice = choice)
+        return HttpResponseRedirect(reverse('poll_view', args=[uuid]))
     def get_context_data(self, uuid, **kwargs):
         ctx = super().get_context_data(**kwargs)
         poll = Poll.objects.get(uuid = uuid)
         votes = Vote.objects.filter(poll = poll)
         results = {poll.options[n-1] if n != 0 else 'poll is wrong':[] for n in range(len(poll.options) + 1)}
         for vote in votes:
-            results[vote.choice].append(vote.user)
+            results[poll.options[vote.choice - 1] if vote.choice != 0 else 'poll is wrong'].append(vote.user)
         ctx['poll_name'] = poll.question
         ctx['poll_results'] = results
         return ctx
