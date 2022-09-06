ec3f5a0721f6e69ac8696bacb24742817a0d02da
asa glenister <asa@animorph.coop>
2022-09-06 16:03:11 +0100

    add models for first two stages

--- apps/project/models.py
+++ apps/project/models.py
@@ -27,7 +27,10 @@ class Project(ClusterableModel):
     name: models.CharField = models.CharField(max_length=200)
     description: models.CharField = models.CharField(max_length=2000)
     tags = ClusterTaggableManager(through=ProjectTag, blank=True)
-    chat: models.ForeignKey = models.ForeignKey('messaging.Chat', null = True, on_delete = models.SET_DEFAULT, default=new_chat) # I'm guessing that if for some reason a chat is deleted, that means we want to purge it and replace it with a new one
+    envision_stage: models.ForeignKey = models.ForeignKey(EnvisionStage, null = True, default = None, on_delete = models.SET_NULL)
+    plan_stage: models.ForeignKey = models.ForeignKey(PlanStage, null = True, default = None, on_delete = models.SET_NULL)
+    # act_stage: models.ForeignKey = models.ForeignKey(ActStage, null = True, default = None, on_delete = models.SET_NULL)
+    # reflect_stage: models.ForeignKey = models.ForeignKey(ReflectStage, null = True, default = None, on_delete = models.SET_NULL)
     def save(self, *args: List[Any], **kwargs: Dict[str,Any]) -> None:
         if (self.slug == ''):
             self.slug = quote(self.name)[:86] + shake_256(str(self.id).encode()).hexdigest(8) # pyre-ignore[16] same
@@ -38,3 +41,23 @@ class ProjectMembership(models.Model):
     user: models.ForeignKey = models.ForeignKey('userauth.CustomUser', on_delete=models.CASCADE)
     owner: models.BooleanField = models.BooleanField(default = False)
     champion: models.BooleanField = models.BooleanField(default = False)
+
+
+# STAGES
+
+class EnvisionStage(models.Model):
+    class Step(models.TextChoices):
+        GET_TO_KNOW = '1', 'get to know'
+        FINALISE_VISION = '2', 'finalise vision'
+        REVIEW = '3', 'review tags and image'
+    chat: models.ForeignKey = models.ForeignKey(Chat, default = new_chat, on_delete = models.SET_DEFAULT)
+    step: models.CharField = models.CharField(max_length = 1, choices = Step.choices, default = Step.GET_TO_KNOW)
+
+class PlanStage(models.Model):
+    general_chat: models.ForeignKey = models.ForeignKey(Chat, default = new_chat, on_delete = models.SET_DEFAULT, related_name = 'general_chat')
+    funding_chat: models.ForeignKey = models.ForeignKey(Chat, default = new_chat, on_delete = models.SET_DEFAULT, related_name = 'funding_chat')
+    location_chat: models.ForeignKey = models.ForeignKey(Chat, default = new_chat, on_delete = models.SET_DEFAULT, related_name = 'location_chat')
+    dates_chat: models.ForeignKey = models.ForeignKey(Chat, default = new_chat, on_delete = models.SET_DEFAULT, related_name = 'dates_chat')
+
+
+
