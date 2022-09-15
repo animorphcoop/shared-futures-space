b7752aa94d4bb44da7fba41b56f74360253af65a
asa glenister <asa@animorph.coop>
2022-09-15 16:21:40 +0100

    test chat view

--- tests/test_polls.py
+++ tests/test_polls.py
@@ -4,6 +4,7 @@ import pytest
 from django.urls import reverse
 from poll.models import Poll, Vote
 from project.models import ProjectMembership
+from messaging.util import send_system_message
 
 def test_create_poll(client, test_user, test_project):
     client.get(reverse('poll_create')) # make sure form doesn't crash while rendering
@@ -15,3 +16,11 @@ def test_create_poll(client, test_user, test_project):
     assert 'answer 1' in new_poll_redirect.content.decode('utf-8')
     assert 'poll is wrong' in new_poll_redirect.content.decode('utf-8')
     assert Poll.objects.get(question = 'is this a test poll?').voter_num == len(ProjectMembership.objects.filter(project = test_project))
+    # check chat view
+    test_project.start_envision()
+    send_system_message(test_project.envision_stage.chat, 'poll', context_poll = Poll.objects.get(question = 'is this a test poll?'))
+    chat_view = client.get(reverse('project_chat', args=[test_project.slug, 'envision', 'general']))
+    assert 'is this a test poll?' in chat_view.content.decode('utf-8')
+    assert 'poll is wrong' in chat_view.content.decode('utf-8')
+
+#def test_vote_poll(client, test_user, test_project):
