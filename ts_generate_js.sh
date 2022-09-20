ffc2ae148799bd4580552a1e9c77465d7880f701
Szczepan Orlowski <sz@animorph.coop>
2022-09-20 16:42:27 +0100

    comments, removed echoes

--- ts_generate_js.sh
+++ ts_generate_js.sh
@@ -3,23 +3,21 @@
 templates_dir=templates
 echo $templates_dir
 allfiles=$(find $templates_dir -type f -name '*.ts')
-#tsc --strict --target es2017 --outDir $templates_dir/ts_output/js $(find $templates_dir -type f -name '*.ts');
 
+#declaring empty before loop
 last_dir=""
 
 for path in $allfiles
 do
-    echo "$path"
     dir_name=${path%/*}  # retain the part before the last slash so we pass files from the same dir
     if [[ "$dir_name" == "$last_dir" ]]; then
       echo "same dir"
     else
-      if [[ "$last_dir" != "" ]]; then #to take out the first loop
+      if [[ "$last_dir" != "" ]]; then #to take out the first iteration
          echo "compiling from ${dir_name}"
          tsc --strict --target es2017 --outDir $templates_dir/ts_output/js $(find $dir_name -type f -name '*.ts');
       fi
       last_dir=$dir_name
-      echo $last_dir
     fi
 done
 
