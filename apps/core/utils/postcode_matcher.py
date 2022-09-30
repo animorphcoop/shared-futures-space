bb599d577402dc29805c39797f791f57e3475e97
asa glenister <asa@animorph.coop>
2022-09-30 16:05:27 +0100

    allow non-uk/ie postcodes

--- apps/core/utils/postcode_matcher.py
+++ apps/core/utils/postcode_matcher.py
@@ -13,10 +13,9 @@ def filter_postcode(typed_postcode):
 
     # get outcode and make upper for a match
     m = re.match(r'([a-zA-Z]{1,2}[0-9][a-zA-Z0-9]?)([0-9][a-zA-Z]{2})?', entered_postcode)
-    if m is None:
-        # not a valid postcode
-        raise ValueError('not a valid postcode: ' + entered_postcode)
-    entered_postcode = m.group(1).upper()
+    if m is not None:
+        # recognised postcode form, normalise
+        entered_postcode = m.group(1).upper()
 
     # look for one with upper - make sure exact match is there,
     # particularly in a situation where people enter a full postcode
