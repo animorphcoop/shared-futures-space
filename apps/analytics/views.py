eca6809c8430b6ed348c235e7a87d8e9a261f652
asa glenister <asa@animorph.coop>
2022-10-12 15:39:38 +0100

    fill remaining analytics requirements

--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -2,6 +2,7 @@
 
 from django.views.generic.base import TemplateView
 
+from userauth.models import CustomUser # pyre-ignore[21]
 from area.models import Area # pyre-ignore[21]
 from project.models import Project, ProjectMembership # pyre-ignore[21]
 from core.utils.tags_declusterer import tag_cluster_to_list # pyre-ignore[21]
@@ -31,18 +32,35 @@ class BarGraph(Graph):
             svg += f'<rect x={30+((width-30)*n/len(self.data))} y={(height-30)-((height-30)*bar/self.scale)} width=10 height={((height-30)*bar/self.scale)}></rect>'
         svg += f'</svg>'
         return svg
+    def total(self):
+        return sum(map(lambda t: t[1], self.data))
+    def average(self):
+        return self.total() / len(self.data)
 
 class AnalyticsView(TemplateView):
     def get_context_data(self, **kwargs: Dict[str,Any]) -> Dict[str,Any]:
         ctx = super().get_context_data(**kwargs)
         ctx['graphs'] = []
+        users_area = [(area.name, len(CustomUser.objects.filter(post_code__area = area))) for area in Area.objects.all()]
+        users_scale = max(map(lambda t: t[1], users_area))
+        ctx['graphs'].append(BarGraph('users/area', users_area, users_scale))
         projects_area = [(area.name, len(Project.objects.filter(area=area))) for area in Area.objects.all()]
         project_scale = max(map(lambda t: t[1], projects_area))
         ctx['graphs'].append(BarGraph('projects/area', projects_area, project_scale))
+        ctx['graphs'].append(BarGraph('projects/stages - overall', [('not begun', len(Project.objects.filter(current_stage = None))),
+                                                                    ('envision', len(Project.objects.filter(current_stage = Project.Stage.ENVISION))),
+                                                                    ('plan', len(Project.objects.filter(current_stage = Project.Stage.PLAN))),
+                                                                    ('act', len(Project.objects.filter(current_stage = Project.Stage.ACT))),
+                                                                    ('reflect', len(Project.objects.filter(current_stage = Project.Stage.REFLECT)))], project_scale))
         for area in Area.objects.all():
             ctx['graphs'].append(BarGraph('projects/stages - ' + area.name, [('not begun', len(Project.objects.filter(area = area, current_stage = None))),
                                                                              ('envision', len(Project.objects.filter(area = area, current_stage = Project.Stage.ENVISION))),
                                                                              ('plan', len(Project.objects.filter(area = area, current_stage = Project.Stage.PLAN))),
                                                                              ('act', len(Project.objects.filter(area = area, current_stage = Project.Stage.ACT))),
                                                                              ('reflect', len(Project.objects.filter(area = area, current_stage = Project.Stage.REFLECT)))], project_scale))
+        swimmers_area = [(area.name, (len(ProjectMembership.objects.filter(project__in = Project.objects.filter(area=area))) / len(Project.objects.filter(area=area)))
+                           if len(Project.objects.filter(area=area)) != 0 else 0)
+                          for area in Area.objects.all()]
+        swimmers_scale = int(max(map(lambda t: t[1], swimmers_area)))+1
+        ctx['graphs'].append(BarGraph('average swimmers/project', swimmers_area, swimmers_scale))
         return ctx
