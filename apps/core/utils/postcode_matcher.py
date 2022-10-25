44fb04a84938c0c64265ee372e731bbf81f8929e
Szczepan Orlowski <sz@animorph.coop>
2022-10-25 11:47:26 +0100

    debugged postcode matcher for add data and postcode assignment

--- apps/core/utils/postcode_matcher.py
+++ apps/core/utils/postcode_matcher.py
@@ -1,15 +1,11 @@
 import re
+from area.models import PostCode
 
-from django.http import HttpResponse
-
-# TODO: They would be loaded via upload_conf with locations
-active_postcodes = ['BT48', 'BT48', 'BT17', 'BT12', 'BT7', 'BT4', 'BT15', 'WD23']
-
-# TODO: make sure on the front the post code is no shorter than 3chars
 
 def filter_postcode(typed_postcode):
     # strip from whitespaces
     entered_postcode = typed_postcode.replace(" ", "")
+    active_postcodes = PostCode.objects.all().values_list('code')
 
     # get outcode and make upper for a match
     m = re.match(r'([a-zA-Z]{1,2}[0-9][a-zA-Z0-9]?)([0-9][a-zA-Z]{2})?', entered_postcode)
@@ -19,12 +15,12 @@ def filter_postcode(typed_postcode):
 
     # look for one with upper - make sure exact match is there,
     # particularly in a situation where people enter a full postcode
-    # but in fact only first 3 chars matter, check first 3
+    # but in fact only first 4 chars matter, check first 4
     for postcode in active_postcodes:
-        if postcode == entered_postcode:
+        if postcode == entered_postcode[0:4]:
             entered_postcode = postcode
-        else:
-            if postcode == entered_postcode[0:3]:
-                entered_postcode = postcode
+
+    if len(entered_postcode) > 4:
+            entered_postcode = entered_postcode[0:4]
 
     return entered_postcode
