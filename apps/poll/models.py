84825a4859bb729738edaaea5f8254794fb91eb8
asa glenister <asa@animorph.coop>
2022-10-26 13:21:22 +0100

    add multiple-choice polls

--- apps/poll/models.py
+++ apps/poll/models.py
@@ -10,6 +10,23 @@ from userauth.models import CustomUser # pyre-ignore[21]
 
 from typing import List, Dict
 
+class BaseVote(models.Model):
+    user: models.ForeignKey = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
+    poll: models.ForeignKey = models.ForeignKey("poll.BasePoll", on_delete = models.CASCADE)
+
+class SingleVote(BaseVote):
+    choice: models.IntegerField = models.IntegerField(null = True, default = None)
+    def clean(self) -> None:
+        cleaned_data = super().clean()
+        # 0 indicates the always-present unask-the-question option
+        if self.choice < 0 or self.choice > len(self.poll.options):
+            raise ValidationError('not a valid choice for that poll (got ' + str(self.choice) + ', expected an integer in 0 - ' + str(len(self.poll.options)) + ')')
+        else:
+            return cleaned_data
+
+class MultipleVote(BaseVote):
+    choice: ArrayField = ArrayField(models.IntegerField())
+
 def validate_poll_options(value: List[str]) -> bool:
     if type(value) == list and all(map(lambda x: type(x) == str, value)):
         return True
@@ -27,10 +44,13 @@ class BasePoll(models.Model):
     def make_votes(self, project) -> None: # pyre-ignore[2] can't import Project for this, see next line
         from project.models import ProjectMembership # pyre-ignore[21] this is considered bad form, but as far as i can tell necessary to avoid a circular import
         for voter in ProjectMembership.objects.filter(project = project):
-            self.vote_kind.objects.create(user = voter.user, poll = self, choice = None)
+            self.vote_kind.objects.create(user = voter.user, poll = self, choice = None if self.vote_kind == SingleVote else [])
+
+class SingleChoicePoll(BasePoll):
+    vote_kind = SingleVote
     @property
     def current_results(self) -> Dict[str,List[CustomUser]]: # pyre-ignore[11]
-        votes = Vote.objects.filter(poll = self, choice__isnull = False)
+        votes = SingleVote.objects.filter(poll = self, choice__isnull = False)
         results = {self.options[n-1] if n != 0 else 'poll is wrong':[] for n in range(len(self.options) + 1)}
         for vote in votes:
             results[self.options[vote.choice - 1] if vote.choice != 0 else 'poll is wrong'].append(vote.user)
@@ -43,7 +63,8 @@ class BasePoll(models.Model):
             self.save()
             return True
         else:
-            if vote_nums[0] > vote_nums[1] + len(Vote.objects.filter(poll = self, choice__isnull = True)):
+            vote_nums = sorted([len(SingleVote.objects.filter(poll = self, choice = option)) for option in range(len(self.options)+1)], reverse = True)
+            if vote_nums[0] > vote_nums[1] + len(SingleVote.objects.filter(poll = self, choice__isnull = True)):
                 # if all remaining votes went to the current second-place option it still wouldn't equal the top option
                 self.closed = True
                 self.save()
@@ -51,25 +72,29 @@ class BasePoll(models.Model):
             else:
                 return False
 
-class SingleChoicePoll(BasePoll):
-    vote_kind = SingleVote
-
 class MultipleChoicePoll(BasePoll):
     vote_kind = MultipleVote
-
-class BaseVote(models.Model):
-    user: models.ForeignKey = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
-    poll: models.ForeignKey = models.ForeignKey(Poll, on_delete = models.CASCADE)
-
-class SingleVote(BaseVote):
-    choice: models.IntegerField = models.IntegerField(null = True, default = None)
-    def clean(self) -> None:
-        cleaned_data = super().clean()
-        # 0 indicates the always-present unask-the-question option
-        if self.choice < 0 or self.choice > len(self.poll.options):
-            raise ValidationError('not a valid choice for that poll (got ' + str(self.choice) + ', expected an integer in 0 - ' + str(len(self.poll.options)) + ')')
+    @property
+    def current_results(self) -> Dict[str,List[CustomUser]]: # pyre-ignore[11]
+        votes = MultipleVote.objects.filter(~models.Q(choice = []), poll = self)
+        results = {self.options[n-1] if n != 0 else 'poll is wrong':[] for n in range(len(self.options) + 1)}
+        for vote in votes:
+            for choice in vote.choice:
+                results[self.options[choice - 1] if choice != 0 else 'poll is wrong'].append(vote.user)
+        return results
+    def check_closed(self) -> bool:
+        if self.closed:
+            return True
+        elif self.expires < timezone.now():
+            self.closed = True
+            self.save()
+            return True
         else:
-            return cleaned_data
-
-class MultipleVote(BaseVote):
-    choice: ArrayField = ArrayField(models.IntegerField)
+            vote_nums = sorted(list(map(len, self.current_results.values())), reverse = True)
+            if vote_nums[0] > vote_nums[1] + len(MultipleVote.objects.filter(poll = self, choice = [])):
+                # if all remaining votes went to the current second-place option it still wouldn't equal the top option
+                self.closed = True
+                self.save()
+                return True
+            else:
+                return False
--- apps/poll/views.py
+++ apps/poll/views.py
@@ -6,29 +6,45 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.handlers.wsgi import WSGIRequest
 from django.utils import timezone
 from django.urls import reverse
-from django.forms import ModelChoiceField, ModelForm
+from django.forms import ChoiceField, ModelChoiceField, ModelForm
 from uuid import UUID
 
 from typing import Dict, Any
 
-from .models import Poll, Vote
+from .models import BasePoll, SingleChoicePoll, MultipleChoicePoll, BaseVote, SingleVote, MultipleVote
 from project.models import Project, ProjectMembership # pyre-ignore[21]
 
 class PollView(TemplateView):
     def post(self, request: WSGIRequest, uuid: UUID) -> HttpResponseRedirect:
-        poll = Poll.objects.get(uuid = uuid)
-        if request.user.is_active and not poll.check_closed() and 'choice' in request.POST and Vote.objects.filter(user = request.user, poll = poll).exists():
+        poll = BasePoll.objects.get(uuid = uuid)
+        if hasattr(poll, 'multiplechoicepoll'):
+            poll = poll.multiplechoicepoll
+        elif hasattr(poll, 'singlechoicepoll'):
+            poll = poll.singlechoicepoll
+        if request.user.is_active and not poll.check_closed() and 'choice' in request.POST and BaseVote.objects.filter(user = request.user, poll = poll).exists():
             try:
                 choice = poll.options.index(request.POST['choice']) + 1
             except ValueError:
                 choice = 0
-            Vote.objects.filter(poll = poll, user = request.user).update(choice = choice)
+            if hasattr(poll, 'multiplechoicepoll'):
+                v = MultipleVote.objects.get(poll = poll, user = request.user)
+                if choice in v.choice:
+                    v.choice.remove(choice)
+                else:
+                    v.choice.append(choice)
+                v.save()
+            elif hasattr(poll, 'singlechoicepoll'):
+                SingleVote.objects.filter(poll = poll, user = request.user).update(choice = choice)
             poll.check_closed()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
     def get_context_data(self, uuid: UUID, **kwargs: Dict[str,Any]) -> Dict[str,Any]:
         ctx = super().get_context_data(**kwargs)
-        poll = Poll.objects.get(uuid = uuid)
-        votes = Vote.objects.filter(poll = poll)
+        poll = BasePoll.objects.get(uuid = uuid)
+        if hasattr(poll, 'multiplechoicepoll'):
+            poll = poll.multiplechoicepoll
+        elif hasattr(poll, 'singlechoicepoll'):
+            poll = poll.singlechoicepoll
+        votes = BaseVote.objects.filter(poll = poll)
         ctx['poll_name'] = poll.question
         ctx['poll_results'] = poll.current_results
         ctx['poll_closed'] = poll.check_closed()
@@ -37,15 +53,19 @@ class PollView(TemplateView):
 
 class PollCreateForm(ModelForm):
     project = ModelChoiceField(queryset=Project.objects.all()) # should only be projects you're a member of
+    kind = ChoiceField(choices = [('SINGLE', 'single-choice'), ('MULTIPLE', 'multiple-choice')])
     class Meta:
-        model = Poll
+        model = SingleChoicePoll
         fields = ['question', 'options', 'expires']
 
 class PollCreateView(CreateView): # pyre-ignore[24]
-    model = Poll
+    model = SingleChoicePoll
     form_class = PollCreateForm
     def form_valid(self, form) -> HttpResponseRedirect: # pyre-ignore[2] - the type of the form argument is some weird private thing that i can't seem to get hold of
-        new_poll = Poll.objects.create(question = form.instance.question, options = form.instance.options, expires = form.instance.expires)
+        if form.cleaned_data['kind'] == 'SINGLE':
+            new_poll = SingleChoicePoll.objects.create(question = form.instance.question, options = form.instance.options, expires = form.instance.expires)
+        elif form.cleaned_data['kind'] == 'MULTIPLE':
+            new_poll = MultipleChoicePoll.objects.create(question = form.instance.question, options = form.instance.options, expires = form.instance.expires)
         new_poll.make_votes(form.cleaned_data['project'])
         return HttpResponseRedirect(reverse('poll_view', args=[new_poll.uuid]))
     def get_success_url(self) -> str:
