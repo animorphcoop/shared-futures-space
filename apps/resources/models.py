b66cda98dee11e9f462a7610e65704492abe811f
Szczepan Orlowski <sz@animorph.coop>
2022-10-31 16:29:25 +0000

    upload conf fixes to do with long links

--- apps/resources/models.py
+++ apps/resources/models.py
@@ -69,7 +69,7 @@ class Resource(ClusterableModel):
         null=False,
     )
     link: models.CharField = models.CharField(
-        max_length=100,
+        max_length=150,
         blank=True,
         null=True,
     )
