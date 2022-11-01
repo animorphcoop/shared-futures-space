d72b506fda2b725a39e603401766f9826af3b59e
Szczepan Orlowski <sz@animorph.coop>
2022-11-01 15:15:21 +0000

    selecting and unselecting tags

--- templates/river/ts/start_river.ts
+++ templates/river/ts/start_river.ts
@@ -1,32 +1,65 @@
 let selectedTags: Array<HTMLElement> = []
+let counterValue: number = 3
+const tagCounterElem: HTMLElement | null = document.getElementById("counter")
+
 
 function selectTag(tagElemId: string, tagName: string) {
     console.log(tagElemId)
     console.log(tagName)
 
-    //let currentResourceElem = document.getElementById(tagElemId)
-    //console.log(currentResourceElem)
-
     const currentTagElem = document.getElementById(tagElemId)
     if (currentTagElem == null || selectedTags == null) return
     if (!selectedTags.includes(currentTagElem)) {
         console.log('new')
-        currentTagElem.classList.add("bg-purple/25")
-        selectedTags.push(currentTagElem)
+        if (counterValue > 0) {
+            currentTagElem.classList.add("bg-purple/25")
+            selectedTags.push(currentTagElem)
+            counterDown(true)
+        }
+
 
     } else {
         console.log('removing')
         console.log(selectedTags.length)
-
-        currentTagElem.classList.remove("bg-purple/25")
-        selectedTags.forEach((item, index) => {
-            if (item === currentTagElem)
-                selectedTags.splice(index, 1)
-        })
+        if (counterValue < 3) {
+            currentTagElem.classList.remove("bg-purple/25")
+            selectedTags.forEach((item, index) => {
+                if (item === currentTagElem)
+                    selectedTags.splice(index, 1)
+            })
+            counterDown(false)
+        }
         console.log(selectedTags.length)
     }
 
-
     //console.log(selectedTags[0])
     console.log('hehehe6')
+}
+
+function counterDown(toDecrease: boolean) {
+
+    if (tagCounterElem == null || counterValue == null) return
+
+    if (toDecrease) {
+        counterValue--
+    } else {
+        counterValue++
+    }
+    console.log(counterValue)
+
+
+    if (counterValue < 1) {
+        if (!tagCounterElem.classList.contains('hidden')) {
+            tagCounterElem.classList.add('hidden')
+        }
+    } else {
+
+        if (tagCounterElem.classList.contains('hidden')) {
+            tagCounterElem.classList.remove('hidden')
+        }
+    }
+    console.log(tagCounterElem.classList)
+    tagCounterElem.innerText = `choose ${counterValue}`
+
+
 }
--- templates/ts_output/js/start_river.js
+++ templates/ts_output/js/start_river.js
@@ -1,28 +1,57 @@
 "use strict";
 let selectedTags = [];
+let counterValue = 3;
+const tagCounterElem = document.getElementById("counter");
 function selectTag(tagElemId, tagName) {
     console.log(tagElemId);
     console.log(tagName);
-    //let currentResourceElem = document.getElementById(tagElemId)
-    //console.log(currentResourceElem)
     const currentTagElem = document.getElementById(tagElemId);
     if (currentTagElem == null || selectedTags == null)
         return;
     if (!selectedTags.includes(currentTagElem)) {
         console.log('new');
-        currentTagElem.classList.add("bg-purple/25");
-        selectedTags.push(currentTagElem);
+        if (counterValue > 0) {
+            currentTagElem.classList.add("bg-purple/25");
+            selectedTags.push(currentTagElem);
+            counterDown(true);
+        }
     }
     else {
         console.log('removing');
         console.log(selectedTags.length);
-        currentTagElem.classList.remove("bg-purple/25");
-        selectedTags.forEach((item, index) => {
-            if (item === currentTagElem)
-                selectedTags.splice(index, 1);
-        });
+        if (counterValue < 3) {
+            currentTagElem.classList.remove("bg-purple/25");
+            selectedTags.forEach((item, index) => {
+                if (item === currentTagElem)
+                    selectedTags.splice(index, 1);
+            });
+            counterDown(false);
+        }
         console.log(selectedTags.length);
     }
     //console.log(selectedTags[0])
     console.log('hehehe6');
+}
+function counterDown(toDecrease) {
+    if (tagCounterElem == null || counterValue == null)
+        return;
+    if (toDecrease) {
+        counterValue--;
+    }
+    else {
+        counterValue++;
+    }
+    console.log(counterValue);
+    if (counterValue < 1) {
+        if (!tagCounterElem.classList.contains('hidden')) {
+            tagCounterElem.classList.add('hidden');
+        }
+    }
+    else {
+        if (tagCounterElem.classList.contains('hidden')) {
+            tagCounterElem.classList.remove('hidden');
+        }
+    }
+    console.log(tagCounterElem.classList);
+    tagCounterElem.innerText = `choose ${counterValue}`;
 }
