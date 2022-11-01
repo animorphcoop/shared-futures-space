d08880b2d889d7e92601fbcad9147c6c1ef87902
Szczepan Orlowski <sz@animorph.coop>
2022-11-01 15:59:37 +0000

    listing tags and submitting, description text area

--- templates/river/ts/start_river.ts
+++ templates/river/ts/start_river.ts
@@ -1,39 +1,48 @@
 let selectedTags: Array<HTMLElement> = []
 let counterValue: number = 3
 const tagCounterElem: HTMLElement | null = document.getElementById("counter")
+const tagsInput = (<HTMLInputElement>document.getElementById("tags_input"))
 
 
 function selectTag(tagElemId: string, tagName: string) {
-    console.log(tagElemId)
-    console.log(tagName)
 
     const currentTagElem = document.getElementById(tagElemId)
-    if (currentTagElem == null || selectedTags == null) return
+    if (currentTagElem == null || selectedTags == null || tagsInput == null) return
+
     if (!selectedTags.includes(currentTagElem)) {
-        console.log('new')
         if (counterValue > 0) {
             currentTagElem.classList.add("bg-purple/25")
             selectedTags.push(currentTagElem)
+
+            if (tagsInput.value == "") {
+                tagsInput.value = `"${tagName}",`
+            } else {
+                tagsInput.value = `${tagsInput.value} "${tagName}", `
+            }
+
+            console.log(tagsInput.value)
             counterDown(true)
         }
 
-
     } else {
-        console.log('removing')
-        console.log(selectedTags.length)
         if (counterValue < 3) {
             currentTagElem.classList.remove("bg-purple/25")
             selectedTags.forEach((item, index) => {
                 if (item === currentTagElem)
                     selectedTags.splice(index, 1)
             })
+            if (tagsInput.value != "") {
+                let toRemove = `"${tagName}",`
+                tagsInput.value = tagsInput.value.replace(toRemove, "")
+
+            }
+            console.log(tagsInput.value)
+
             counterDown(false)
+
         }
-        console.log(selectedTags.length)
     }
 
-    //console.log(selectedTags[0])
-    console.log('hehehe6')
 }
 
 function counterDown(toDecrease: boolean) {
@@ -45,7 +54,6 @@ function counterDown(toDecrease: boolean) {
     } else {
         counterValue++
     }
-    console.log(counterValue)
 
 
     if (counterValue < 1) {
@@ -58,7 +66,6 @@ function counterDown(toDecrease: boolean) {
             tagCounterElem.classList.remove('hidden')
         }
     }
-    console.log(tagCounterElem.classList)
     tagCounterElem.innerText = `choose ${counterValue}`
 
 
--- templates/ts_output/js/start_river.js
+++ templates/ts_output/js/start_river.js
@@ -2,35 +2,40 @@
 let selectedTags = [];
 let counterValue = 3;
 const tagCounterElem = document.getElementById("counter");
+const tagsInput = document.getElementById("tags_input");
 function selectTag(tagElemId, tagName) {
-    console.log(tagElemId);
-    console.log(tagName);
     const currentTagElem = document.getElementById(tagElemId);
-    if (currentTagElem == null || selectedTags == null)
+    if (currentTagElem == null || selectedTags == null || tagsInput == null)
         return;
     if (!selectedTags.includes(currentTagElem)) {
-        console.log('new');
         if (counterValue > 0) {
             currentTagElem.classList.add("bg-purple/25");
             selectedTags.push(currentTagElem);
+            if (tagsInput.value == "") {
+                tagsInput.value = `"${tagName}",`;
+            }
+            else {
+                tagsInput.value = `${tagsInput.value} "${tagName}", `;
+            }
+            console.log(tagsInput.value);
             counterDown(true);
         }
     }
     else {
-        console.log('removing');
-        console.log(selectedTags.length);
         if (counterValue < 3) {
             currentTagElem.classList.remove("bg-purple/25");
             selectedTags.forEach((item, index) => {
                 if (item === currentTagElem)
                     selectedTags.splice(index, 1);
             });
+            if (tagsInput.value != "") {
+                let toRemove = `"${tagName}",`;
+                tagsInput.value = tagsInput.value.replace(toRemove, "");
+            }
+            console.log(tagsInput.value);
             counterDown(false);
         }
-        console.log(selectedTags.length);
     }
-    //console.log(selectedTags[0])
-    console.log('hehehe6');
 }
 function counterDown(toDecrease) {
     if (tagCounterElem == null || counterValue == null)
@@ -41,7 +46,6 @@ function counterDown(toDecrease) {
     else {
         counterValue++;
     }
-    console.log(counterValue);
     if (counterValue < 1) {
         if (!tagCounterElem.classList.contains('hidden')) {
             tagCounterElem.classList.add('hidden');
@@ -52,6 +56,5 @@ function counterDown(toDecrease) {
             tagCounterElem.classList.remove('hidden');
         }
     }
-    console.log(tagCounterElem.classList);
     tagCounterElem.innerText = `choose ${counterValue}`;
 }
