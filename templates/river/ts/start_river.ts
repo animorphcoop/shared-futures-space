bc455335d956dad967e2f244684f04a1307ae529
Szczepan Orlowski <sz@animorph.coop>
2022-11-01 14:58:36 +0000

    adding and removing from array

--- templates/river/ts/start_river.ts
+++ templates/river/ts/start_river.ts
@@ -1,3 +1,5 @@
+let selectedTags: Array<HTMLElement> = []
+
 function selectTag(tagElemId: string, tagName: string) {
     console.log(tagElemId)
     console.log(tagName)
@@ -6,6 +8,25 @@ function selectTag(tagElemId: string, tagName: string) {
     //console.log(currentResourceElem)
 
     const currentTagElem = document.getElementById(tagElemId)
-    if (currentTagElem == null) return
-    currentTagElem.classList.add("bg-purple/25")
+    if (currentTagElem == null || selectedTags == null) return
+    if (!selectedTags.includes(currentTagElem)) {
+        console.log('new')
+        currentTagElem.classList.add("bg-purple/25")
+        selectedTags.push(currentTagElem)
+
+    } else {
+        console.log('removing')
+        console.log(selectedTags.length)
+
+        currentTagElem.classList.remove("bg-purple/25")
+        selectedTags.forEach((item, index) => {
+            if (item === currentTagElem)
+                selectedTags.splice(index, 1)
+        })
+        console.log(selectedTags.length)
+    }
+
+
+    //console.log(selectedTags[0])
+    console.log('hehehe6')
 }
--- templates/ts_output/js/start_river.js
+++ templates/ts_output/js/start_river.js
@@ -1,11 +1,28 @@
 "use strict";
+let selectedTags = [];
 function selectTag(tagElemId, tagName) {
     console.log(tagElemId);
     console.log(tagName);
     //let currentResourceElem = document.getElementById(tagElemId)
     //console.log(currentResourceElem)
     const currentTagElem = document.getElementById(tagElemId);
-    if (currentTagElem == null)
+    if (currentTagElem == null || selectedTags == null)
         return;
-    currentTagElem.classList.add("bg-purple/25");
+    if (!selectedTags.includes(currentTagElem)) {
+        console.log('new');
+        currentTagElem.classList.add("bg-purple/25");
+        selectedTags.push(currentTagElem);
+    }
+    else {
+        console.log('removing');
+        console.log(selectedTags.length);
+        currentTagElem.classList.remove("bg-purple/25");
+        selectedTags.forEach((item, index) => {
+            if (item === currentTagElem)
+                selectedTags.splice(index, 1);
+        });
+        console.log(selectedTags.length);
+    }
+    //console.log(selectedTags[0])
+    console.log('hehehe6');
 }
