d47ad1a8d5833927840eaeef34d8a94fce205127
Szczepan Orlowski <sz@animorph.coop>
2022-11-03 17:26:20 +0000

    added link passed to link button on dashboard-resource

---
+++ templates/ts/copy_link.ts
@@ -0,0 +1,20 @@
+function copyLink(passedUrl: string) {
+    console.log(window.location)
+    const currentLocation = window.location.toString()
+    const locationRoot = currentLocation.split('/', 3).join("//")
+    console.log(locationRoot);
+    //navigator.clipboard.writeText(window.location.href); - requires HTTPS or localhost
+    const url = document.createElement('input');
+    if (passedUrl) {
+        console.log('goti')
+        url.setAttribute('value', locationRoot+passedUrl);
+
+    } else {
+        url.setAttribute('value', currentLocation);
+
+    }
+    document.body.appendChild(url);
+    url.select();
+    document.execCommand('copy');
+    document.body.removeChild(url);
+}
