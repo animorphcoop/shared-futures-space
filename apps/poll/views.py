4535f6ad93a10bc88ed20e4d8b4b308ae200b191
asa glenister <asa@animorph.coop>
2022-11-04 13:09:44 +0000

    redo something lost by a mistaken merge

--- apps/poll/views.py
+++ apps/poll/views.py
@@ -80,10 +80,9 @@ class PollCreateView(CreateView): # pyre-ignore[24]
     form_class = PollCreateForm
     def form_valid(self, form) -> HttpResponseRedirect: # pyre-ignore[2] - the type of the form argument is some weird private thing that i can't seem to get hold of
         if form.cleaned_data['kind'] == 'SINGLE':
-            new_poll = SingleChoicePoll.objects.create(question = form.instance.question, options = form.instance.options, expires = form.instance.expires, created_by = self.request.user)
+            new_poll = SingleChoicePoll.objects.create(question = form.instance.question, options = form.instance.options, expires = form.instance.expires, created_by = self.request.user, river = form.cleaned_data['river'])
         elif form.cleaned_data['kind'] == 'MULTIPLE':
-            new_poll = MultipleChoicePoll.objects.create(question = form.instance.question, options = form.instance.options, expires = form.instance.expires, created_by = self.request.user)
-        new_poll.make_votes(form.cleaned_data['river'])
+            new_poll = MultipleChoicePoll.objects.create(question = form.instance.question, options = form.instance.options, expires = form.instance.expires, created_by = self.request.user, river = form.cleaned_data['river'])
         return HttpResponseRedirect(reverse('poll_view', args=[new_poll.uuid]))
     def get_success_url(self) -> str:
         return reverse('poll_view', args=[self.object.uuid]) # pyre-ignore[16]
