c4ad1f71bd488c9531eb8fa9239c8669c663f047
Szczepan Orlowski <sz@animorph.coop>
2022-11-23 17:12:50 +0000

    filter out case studies without image

--- apps/resources/templatetags/case_studies.py
+++ apps/resources/templatetags/case_studies.py
@@ -11,7 +11,8 @@ register = template.Library()
 def get_case_studies() -> List[str]:
     case_studies = []
     for case_study in CaseStudy.objects.all():
-            case_studies.append(case_study)
+            if case_study.case_study_image is not None:
+                case_studies.append(case_study)
     shuffle(case_studies)
     return case_studies[:3]
 
