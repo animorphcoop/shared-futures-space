bc07c00cd9a1159aae0f72735a4455e10347afec
asa glenister <asa@animorph.coop>
2022-12-05 12:18:51 +0000

    move secret keys into seperate file

--- sfs/settings/local.py
+++ sfs/settings/local.py
@@ -1,9 +1,7 @@
 # pyre-strict
 from typing import List, Optional
 from .base import INSTALLED_APPS
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY: str = '_J(I)NBO*OYNO(M0[mun89pyP (8h8 9)M*MJ<"{KKT FUTYg iuytyp9||\ReU'
+from .secrets import SECRET_KEY, WEATHER_API_KEY, EMAIL_HOST_PASSWORD, FACEBOOK_CLIENT_ID, FACEBOOK_SECRET, GOOOGLE_CLIENT_ID, GOOGLE_SECRET
 
 SITE_ID = 1
 SITE_DOMAIN = 'sharedfutures.space'
@@ -18,20 +16,18 @@ AUTHENTICATION_BACKENDS: List[str] = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-# !!!!! SECURITY WARNING !!!!!
-# these are our actual creds, they MUST be changed on the relevant accounts before publishing the repo
 SOCIALACCOUNT_PROVIDERS = {
     'facebook': {
         'APP': {
-            'client_id': '780510723095271',
-            'secret': '28cd8f0f9c8bfaae6dd4a732c56fa8e6',
+            'client_id': FACEBOOK_CLIENT_ID,
+            'secret': FACEBOOK_SECRET,
             'key': ''
         }
     },
     'google': {
         'APP': {
-            'client_id': '952892024794-dr53p12mssmv41j362o46h92nktmh5b6.apps.googleusercontent.com',
-            'secret': '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',
+            'client_id': GOOGLE_CLIENT_ID,
+            'secret': GOOGLE_SECRET,
             'key': ''
         }
     }
@@ -46,12 +42,10 @@ ACCOUNT_EMAIL_REQUIRED: bool = True
 ACCOUNT_EMAIL_VERIFICATION: str = 'mandatory'
 ACCOUNT_USERNAME_REQUIRED: bool = False
 
-# !!!!! SECURITY WARNING !!!!!
-# these are our actual creds, they MUST be changed on the relevant accounts before publishing the repo
 EMAIL_HOST: str = 'mail.webarch.net'
 EMAIL_PORT: int = 465
 EMAIL_HOST_USER: str = 'sfs_mailer@animorph.coop'
-EMAIL_HOST_PASSWORD: str = '7{zjA+b!xWLe5i>C[)U6jOx<gQe(x9g'
+EMAIL_HOST_PASSWORD: str = EMAIL_HOST_PASSWORD # from secrets.py
 EMAIL_USE_TLS: bool = False
 EMAIL_USE_SSL: bool = True
 DEFAULT_FROM_EMAIL: str = EMAIL_HOST_USER
@@ -71,7 +65,3 @@ SOCIALACCOUNT_ADAPTER = 'userauth.adapters.CustomSocialAccountAdapter'
 CELERY_ACCEPT_CONTENT: List[str] = ['pickle']
 CELERY_TASK_SERIALIZER: str = 'pickle'
 CELERY_RESULT_SERIALIZER: str = 'pickle'
-
-# weather map api for dashboard
-
-WEATHER_API_KEY = "<insert weather api key>"
