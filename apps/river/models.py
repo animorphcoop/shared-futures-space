6b9e74498dac44bc74555bd84b274f6da88ae705
Szczepan Orlowski <sz@animorph.coop>
2022-12-19 21:22:47 +0000

    added the rest of messages for today

--- apps/river/models.py
+++ apps/river/models.py
@@ -167,7 +167,6 @@ class River(ClusterableModel):
             send_system_message(kind='salmon_wizard', chat=self.envision_stage.general_chat, context_river=self, text="Once there are 3 swimmers in the river, you can shape the vision together. Discuss the current description and make sure it captures your collective vision for this project.")
             send_system_message(kind='salmon_wizard', chat=self.envision_stage.general_chat, context_river=self,
                                 text="When you’re ready, the river starter can launch a poll using the poll icon in the bottom right. Here they can edit the original description to include any changes that have been discussed. If the poll passes, you will move to the planning stage.")
-            #send_system_message(kind='salmon_wizard', chat=self.envision_stage.general_chat, context_river=self, text="")
             self.save()
 
     def start_plan(self) -> None:
@@ -176,6 +175,21 @@ class River(ClusterableModel):
             self.current_stage = self.Stage.PLAN
             self.plan_stage = PlanStage.objects.create()
             self.plan_stage.save()
+            send_system_message(kind='salmon_wizard', chat=self.plan_stage.general_chat, context_river=self,
+                                text="Welcome to the plan stage, here you can discuss and make plans for how you'll achieve your vision. The general tab is a space to talk about anything not covered in the other tabs.")
+
+            send_system_message(kind='salmon_wizard', chat=self.plan_stage.general_chat, context_river=self,
+                                text="To progress to the act stage, agree on your plans by passing polls in each of the money, place, and time chats. Once those have passed, the river starter can open a final poll in the general tab. If the general poll passes, you will move to the act stage.")
+            send_system_message(kind='salmon_wizard', chat=self.plan_stage.money_chat, context_river=self,
+                                text="How much will it cost to acheive your vision? Who is creating the budget? Do you need to apply for funding? The money tab is a place to discuss all finance related aspects of your river.")
+            send_system_message(kind='salmon_wizard', chat=self.plan_stage.place_chat, context_river=self,
+                                text="Where will your project take place? Do you need to get permission? Who's going to book the venue? The place tab is a place to discuss all location-based aspects of your river.")
+            send_system_message(kind='salmon_wizard', chat=self.plan_stage.time_chat, context_river=self,
+                                text="Are there any deadlines you need to meet? What date will you be working towards? Are there opening times you need to factor in? The time tab is a place to discuss all time related aspects of your river.")
+
+
+
+
             self.save()
 
     def make_plan_general_poll(self) -> None:
@@ -255,6 +269,19 @@ class River(ClusterableModel):
             send_system_message(self.act_stage.time_chat, 'poll', context_poll=self.plan_stage.time_poll)
             send_system_message(self.act_stage.time_chat, 'poll', context_poll=self.act_stage.time_poll)
             self.act_stage.save()
+            send_system_message(kind='salmon_wizard', chat=self.act_stage.general_chat, context_river=self,
+                                text="Time to act! You can share updates on how the plans are being carried out as you go, to document the flow, and discuss any changes that arise.")
+
+            send_system_message(kind='salmon_wizard', chat=self.act_stage.general_chat, context_river=self,
+                                text="As each aspect of the project concludes, you can use the polls in each tab to record this. When all polls have passed, you will progress to the reflect stage.")
+            send_system_message(kind='salmon_wizard', chat=self.act_stage.money_chat, context_river=self,
+                                text="Are the finances going according to plan? Are you over- or under-budget? Share and discuss money-related updates here.")
+            send_system_message(kind='salmon_wizard', chat=self.act_stage.place_chat, context_river=self,
+                                text="How is the space shaping up? Do you need to change location? Share and discuss place-related updates here.")
+            send_system_message(kind='salmon_wizard', chat=self.act_stage.time_chat, context_river=self,
+                                text="Is everything running on time? Are you overcoming any delays? Share and discuss time-related updates here.")
+
+
             self.save()
 
     def start_reflect(self) -> None:
@@ -278,6 +305,8 @@ class River(ClusterableModel):
                                                    tag_a != tag_b and tag_a > tag_b]))),
                 expires=timezone.now() + timezone.timedelta(days=7),
                 river=self)
+            send_system_message(kind='salmon_wizard', chat=self.reflect_stage.general_chat, context_river=self,
+                                text="Welcome to the reflect stage! I trust your visions have become realities. You can reflect together here on what went well and would could have gone better, to create a record for yourselves and others to learn from. You can also track which, if any, of the resources I offered on this river were helpful to you. Congratulations on making it to the end of the river!")
             self.reflect_stage.save()
 
     def finish(self) -> None:
