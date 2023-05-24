4f1020dc76fdc50b7fa3a48dac8f5f2791b6451e
Theodore Keloglou <theodore@animorph.coop>
2023-05-24 12:54:09 +0100

    add contact form

---
+++ apps/dashboard/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.core import mail
+from django.conf import settings
+
+
+class ContactForm(forms.Form):
+    message = forms.CharField(label="Message", widget=forms.Textarea)
+    subject = forms.CharField(label="Subject", max_length=100)
+    email = forms.EmailField(label="Email", max_length=100)
+
+    def send_email(self):
+        subject_dict = {
+            "resources": "Suggest resources",
+            "feedback": "Send your feedback",
+            "rollout": "Enquire about roll out in your area",
+            "bug": "Report a bug",
+        }
+        subject_verbose = subject_dict[self.cleaned_data["subject"]]
+        mail.send_mail(
+            subject=f"SFS Contact Form submission from: {self.cleaned_data['email']}",
+            message=subject_verbose + "\n\n" + self.cleaned_data["message"],
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=["we@animorph.coop"],
+        )
