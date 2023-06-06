a0faa0c1841cc3701f85b80f74a23f86679a038f
Theodore Keloglou <theodore@animorph.coop>
2023-06-06 17:37:44 +0100

    add direct messages on daily messages task

--- apps/userauth/tasks.py
+++ apps/userauth/tasks.py
@@ -18,7 +18,34 @@ def send_after(duration: float, message: EmailMessage) -> None:
     sleep(duration)
     message.send()
 
-def get_message_data():
+
+def get_direct_message_data():
+    count_dict = defaultdict(int)
+    receivers_dict = defaultdict(set)
+
+    # iterate over all user pairs to find all DM chats
+    user_pair_list = UserPair.objects.filter(blocked=False)
+    for user_pair in user_pair_list:
+        # iterate over all today's messages and see if they belong in the current chat
+        today = timezone.now().date()
+        message_list = Message.objects.filter(
+            timestamp__date=today,
+            chat=user_pair.chat,
+        )
+        for message in message_list:
+            # for every message, save receiver
+            if message.sender == user_pair.user1:
+                receiver = user_pair.user2
+            else:
+                receiver = user_pair.user1
+            count_dict[receiver] += 1
+            receivers_dict[receiver].add(message.sender.display_name)
+
+    # return a tuple of two dicts
+    return count_dict, receivers_dict
+
+
+def get_river_message_data():
     """
     Returns two tuples with info about each users new messages. One about
     message count and another with the river titles with new messages.
@@ -60,13 +87,15 @@ def get_message_data():
         # for each member of this river,
         # we add the info we need to the dicts that will be returned
         for membership in RiverMembership.objects.filter(river=river):
-            today = timezone.now().date()
 
             # skip user if they have logged in today
             if has_visited_today(membership.user):
                 continue
 
-            message_list = Message.objects.filter(timestamp__date=today, chat__in=chat_list)
+            today = timezone.now().date()
+            message_list = Message.objects.filter(
+                timestamp__date=today, chat__in=chat_list
+            )
             for message in message_list:
                 count_dict[membership.user] += 1
                 river_dict[membership.user].add(membership.river.title)
@@ -74,25 +103,42 @@ def get_message_data():
     # return a tuple of two dicts
     return count_dict, river_dict
 
+
 @shared_task
 def send_daily_messages() -> None:
-    count_dict, river_dict = get_message_data()
-    for user in count_dict:
-        message_count = count_dict[user]
-        river_set = river_dict[user]
-        river_string = '\n'.join(river_set)
+    email_list = []
+    direct_count_dict, direct_message_dict = get_direct_message_data()
+    river_count_dict, river_message_dict = get_river_message_data()
+
+    for user in river_count_dict:
+        message_count = river_count_dict[user]
+
+        # add direct messages in total count
+        if user in direct_count_dict:
+            message_count += direct_count_dict[user]
+
+        river_set = river_message_dict[user]
+        river_string = "\n".join(river_set)
         body = f"""
 {message_count} new message(s) waiting for you on Shared Futures Space.
 
 Rivers with messages:
 {river_string}
         """
-        print(body)
-        breakpoint()
-        email = EmailMessage(
-            subject=f"New messages for you on Shared Futures Space",
-            body=body,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            to=[user.email],
+
+        # add direct messages that user received, if they exist
+        if user in direct_message_dict:
+            senders_set = direct_message_dict[user]
+            senders_string = "\n".join(senders_set)
+            body += f"\n\nUsers who messaged you:\n{senders_string}\n"
+
+        email_list.append(
+            EmailMessage(
+                subject=f"New messages for you on Shared Futures Space",
+                body=body,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                to=[user.email],
+            )
         )
-        email.send()
+
+    send_mass_mail(email_list)
