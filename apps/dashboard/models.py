5ec86f22ce3b6f06fd6752fa26c0d389a440aac0
Theodore Keloglou <theodore@animorph.coop>
2023-06-16 12:53:49 +0100

    add wizard model

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Wizard(models.Model):
+    """
+    Wizard is a singleton model. It only has one row, connected to the
+    current SFS app instance.
+    """
+
+    started_at = models.DateTimeField(default=timezone.now)
+    completed_at = models.DateTimeField(blank=True, null=True)
