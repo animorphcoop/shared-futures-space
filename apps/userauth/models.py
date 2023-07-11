bea957883e8f93a5ad31d8a2f29c81fe261c7a7c
asa glenister <asa@animorph.coop>
2023-07-11 12:15:39 +0100

    can now edit users in wagtail!

--- apps/userauth/models.py
+++ apps/userauth/models.py
@@ -10,7 +10,6 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from messaging.models import Chat
 
-
 class Organisation(models.Model):
     uuid: models.UUIDField = models.UUIDField(default=uuid4)
     name: models.CharField = models.CharField(max_length=100)
@@ -62,6 +61,11 @@ class CustomUser(AbstractUser):
         Organisation, default=None, null=True, on_delete=models.SET_NULL
     )
     postcode_changes: models.IntegerField = models.IntegerField(default=3)
+
+    was_active: models.BooleanField = models.BooleanField(default = True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username'] # this just lets us use createsuperuser
+    
 
     class Meta:
         ordering: List[str] = ["display_name"]
