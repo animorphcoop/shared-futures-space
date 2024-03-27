b3ff929e2f6702804acb09c3f2bff25edb83709a
Nick Sellen <git@nicksellen.co.uk>
2024-03-27 19:00:45 +0000

    Add docs explaining the LocationInput current_user

--- apps/core/widgets.py
+++ apps/core/widgets.py
@@ -16,7 +16,29 @@ class LocationInput(MultiWidget):
     """Widget to set coordinates, precision, and zoom at once
 
     Goes hand-in-hand with LocationField
-    Precision and zoom are both optional
+    Precision and Zoom are both optional
+
+    The current_user bit is slightly funky so worth explaining.
+
+    We use it in this widget to set the default map center/zoom
+    to their area.
+
+    It needs to be passed down through many layers to get
+    down to the widget template file.
+
+    To get it from the view all the way you need to override:
+
+    - "get_form_kwargs" in the view to include current_user
+      see RiverStartWizardView for example
+
+    - the form constructor, to receive the current_user
+      see CreateRiverFormStep2 for example
+
+    - pass it to the widget context
+      see this class for example
+
+    By default, we set it to None so the widget still works
+    if you don't need the current_user
     """
 
     template_name = "river/widgets/location.html"
@@ -25,6 +47,7 @@ class LocationInput(MultiWidget):
         self.enable_precision = enable_precision
         self.enable_zoom = enable_zoom
         self.current_user = None
+
         super().__init__(
             widgets=(
                 # co-ordinates
