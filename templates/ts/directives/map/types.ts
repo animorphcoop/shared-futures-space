a26be9db69f6d70396f0aac0480dcb495ddda170
Nick Sellen <git@nicksellen.co.uk>
2024-05-21 16:54:52 +0100

    Show currently selected map marker

--- templates/ts/directives/map/types.ts
+++ templates/ts/directives/map/types.ts
@@ -1,38 +1,39 @@
-import CSS from "csstype";
-import { PaddingOptions, RequireAtLeastOne } from "maplibre-gl";
+import CSS from "csstype"
+import { PaddingOptions, RequireAtLeastOne } from "maplibre-gl"
 
 export type MapCoordinates = [number, number] // longitude, latitude
 
-export type MarkerType = 'river' | 'river-finished' | 'resource' | 'idea'
+export type MarkerType = "river" | "river-finished" | "resource" | "idea"
 
 export interface MapMarker {
-    name: string
-    type: MarkerType
-    coordinates: MapCoordinates
-    approximate?: boolean
+  name: string
+  type: MarkerType
+  coordinates: MapCoordinates
+  approximate?: boolean
 }
 
 export type Padding = RequireAtLeastOne<PaddingOptions>
 
 export interface MapOptions {
-    types?: MarkerType[]
-    home?: {
-        center: MapCoordinates
-        zoom: number
-    }
-    markers?: MapMarker[]
-    center?: MapCoordinates
-    zoom?: number
-    cursor?: CSS.Properties['cursor']
-    padding?: Padding
-    autofit?: boolean
-    filterControl?: boolean
-    // Zooming on scroll can be annoying on more
-    // content-like pages
-    disableScrollZoom?: boolean
+  types?: MarkerType[]
+  home?: {
+    center: MapCoordinates
+    zoom: number
+  }
+  markers?: MapMarker[]
+  selectedMarker?: MapMarker
+  center?: MapCoordinates
+  zoom?: number
+  cursor?: CSS.Properties["cursor"]
+  padding?: Padding
+  autofit?: boolean
+  filterControl?: boolean
+  // Zooming on scroll can be annoying on more
+  // content-like pages
+  disableScrollZoom?: boolean
 }
 
 export interface CurrentOptions {
-    options: MapOptions
-    mapCentre?: MapCoordinates
+  options: MapOptions
+  mapCentre?: MapCoordinates
 }
