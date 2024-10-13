e272deea2058a21e481079f507f587a7d7db46eb
nekton <sz@animorph.coop>
2024-10-14 00:36:22 +0100

    stages and polls review

--- apps/river/views.py
+++ apps/river/views.py
@@ -329,20 +329,22 @@ class RiverChatView(ChatView):
                              or (chat_poll.closed and not chat_poll.passed),
                 "stage_ref": stage_ref,
                 # TODO: Write in len(members) > 2  rather than handling in template with members|length>2
-                "poll_possible": (
-                        (kwargs["stage"] == "envision" or (chat_poll is None or not chat_poll.passed))
-                        or (
-                                kwargs["stage"] != "reflect"
-                                and (kwargs["topic"] != "general"
-                                     or (kwargs["topic"] == "general"
-                                         and stage_ref.money_poll
-                                         and stage_ref.money_poll.passed
-                                         and stage_ref.place_poll
-                                         and stage_ref.place_poll.passed
-                                         and stage_ref.time_poll
-                                         and stage_ref.time_poll.passed)
-                                     )
-                        )
+                "poll_possible": True
+                #TODO -  and (chat_poll is None or not chat_poll.passed)) on `envision` enables to rerun the poll but in plan hides envision, we need both
+                if kwargs["stage"] == "envision" and (not chat_poll.passed)
+                else (
+                    False
+                    if kwargs["stage"] == "reflect"
+                    else (kwargs["topic"] != "general")
+                    or (
+                        kwargs["topic"] == "general"
+                        and stage_ref.money_poll
+                        and stage_ref.money_poll.passed
+                        and stage_ref.place_poll
+                        and stage_ref.place_poll.passed
+                        and stage_ref.time_poll
+                        and stage_ref.time_poll.passed
+                    )
                 ),
                 "starters": RiverMembership.objects.filter(
                     river=river, starter=True
